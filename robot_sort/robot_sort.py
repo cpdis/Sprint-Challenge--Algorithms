59c62243ca248c69fbb467583e00ca367a362f19
Colin Dismuke <colin.dismuke@gmail.com>
2019-05-31 21:30:37 -0500

    Finished sorting algorithm

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -126,6 +126,9 @@ class SortingRobot:
                     self.move_left()
                     self.swap_item()
                     self.move_right()
+            if not self.light_is_on():
+                while self.can_move_left():
+                    self.move_left()
 
 
 if __name__ == "__main__":
